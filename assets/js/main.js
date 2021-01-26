6b62fc42bb71bfbc6b03d8b8b22135b5b37eada0
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-01-26 19:45:36 +0100

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function openMenu (){
+  let menu = document.getElementById("menu-list");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+function closeMenu (){
+  let menu = document.getElementById("menu-list");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  }
+}
+
+
